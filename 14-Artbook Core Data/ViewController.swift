7cab2b0c2ba09263244fba439416a6dc71a0ce23
selcuk baki <selcukbaki@windowslive.com>
2021-08-19 21:53:07 +0300

    Initial Commit

---
+++ 14-Artbook Core Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  14-Artbook Core Data
+//
+//  Created by Selcuk Baki on 19/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
